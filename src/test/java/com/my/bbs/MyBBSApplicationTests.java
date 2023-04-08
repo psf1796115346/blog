4f5a9ecdd6168a9cad9554d93a56694c5b3b8c37
我是个笨比 <1796115346@qq.com>
2023-04-08 14:00:40 +0800

    commit again

---
+++ src/test/java/com/my/bbs/MyBBSApplicationTests.java
@@ -0,0 +1,43 @@
+package com.my.bbs;
+
+import com.my.bbs.service.BBSUserService;
+import com.my.bbs.util.PatternUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+
+@SpringBootTest
+class MyBBSApplicationTests {
+	@Autowired
+	BBSUserService bbsUserService;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Test
+	void testDataSource(){
+		System.out.println(dataSource!=null);
+		System.out.println(dataSource);
+	}
+
+	@Test
+	void testPatternUtil(){
+		System.out.println(PatternUtil.isEmail("1034683568@qq.com"));
+		System.out.println(PatternUtil.isEmail("zhenfeng@foxmail.com"));
+		System.out.println(PatternUtil.isEmail("1034683568@163.com"));
+		System.out.println(PatternUtil.isEmail("1034683568@sina.cn"));
+		System.out.println(PatternUtil.isEmail("1034683568@163"));
+		System.out.println(PatternUtil.isEmail("@163.com"));
+	}
+
+	@Test
+	void test1(){
+		System.out.println(bbsUserService.getSubscribeLists(1L));
+	}
+}
